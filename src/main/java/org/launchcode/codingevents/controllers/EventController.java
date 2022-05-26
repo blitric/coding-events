73dcc082f0d315daa8bb4d5845e3fa5dffa9b629
Alma <alma.blitric@gmail.com>
2022-05-25 23:01:39 -0500

    completed model binding practice

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,15 +1,19 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static ArrayList<String> events = new ArrayList<>();
+    //private static ArrayList<String> events = new ArrayList<>();
+   // private static List<Event> events = new ArrayList<>();
     @GetMapping
     public String displayAllEvents(Model model) {
         /*ArrayList<String> events = new ArrayList<>();
@@ -17,7 +21,7 @@ public class EventController {
         events.add("event2");
         events.add("event3");
         model.addAttribute("events", events); */
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
 return "events/index";
     }
 
@@ -28,8 +32,11 @@ return "events/index";
 
 
     @PostMapping("create")
-    public String createEvent( @RequestParam String eventName) {
-        events.add(eventName);
+    //public String createEvent( @RequestParam String eventName, @RequestParam String eventDescription) {
+    public String createEvent( @ModelAttribute Event newEvent) {
+        //events.add(new Event(eventName, eventDescription));
+        //EventData.add(new Event(eventName, eventDescription));
+        EventData.add(newEvent);
         return "redirect:";
 
     }
